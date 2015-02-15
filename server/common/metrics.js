2b3d37e3eacd6f361bf27180c89064e41f2ed385
ksheth <k.sheth@gmail.com>
2015-02-15 23:12:08 +0530

    better metrics update

--- server/common/metrics.js
+++ server/common/metrics.js
@@ -8,6 +8,8 @@ var MongoClient = require('mongodb').MongoClient;
 var UserAgent = require('useragent');
 var mongodb;
 
+var cache = {};
+
 var connect = function (mongouri) {
     return new Promise(function (resolve, reject) {
         MongoClient.connect(mongouri, {}, function (err, db) {
@@ -24,7 +26,7 @@ var connect = function (mongouri) {
 var enrichWithStat = function (s) {
     s.count = 0;
     s.avg = 0;
-    s.min = 0;
+    s.min = 99999999999;
     s.max = 0;
     s.statusCode = {'0#': 1};
     return s;
@@ -44,35 +46,7 @@ var newStatArr = function (size, id, incr) {
     return ret;
 };
 
-var newDoc = function (params, id) {
-    var ret = {
-        _id: id,
-        host: params.host,
-        path: params.path,
-        method: params.method
-    };
-    if (params.user) {
-        ret.user = params.user;
-        ret.type = 'userStats';
-        ret.ua = 'tbd';
-        return enrichWithStat(ret);
-    } else {
-        ret.year = params.year;
-        ret.type = 'combinedStats';
-        ret = enrichWithStat(ret);
-        ret.months = newStatArr(12, 'm', true);
-        var daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        _.forEach(ret.months, function (month) {
-            month.days = newStatArr(daysInMonth[month.m-1], 'd', true);
-            _.forEach(month.days, function (day) {
-                day.hours = newStatArr(24, 'h', false);
-            });
-        });
-        return ret;
-    }
-};
-
-var update = function (stats, ua, statusCode, elapsed) {
+var updNewDoc = function (stats, ua, statusCode, elapsed) {
     _.forEach(stats, function (stat) {
         if (!_.isUndefined(stat.ua)) {
             stat.ua = ua.toString();
@@ -91,44 +65,103 @@ var update = function (stats, ua, statusCode, elapsed) {
     });
 };
 
+var updExistingDoc = function(prefixStr, ua, statusCode, elapsed) {
+    var ret = {
+        $inc: {},
+        $min: {},
+        $max: {}
+    };
+    _.forEach(prefixStr, function (prefix) {
+        ret.$inc[prefix + 'count'] = 1;
+        ret.$inc[prefix + 'statusCode.' + statusCode] = 1;
+        ret.$min[prefix + 'min'] = elapsed;
+        ret.$max[prefix + 'max'] = elapsed;
+        //https://jira.mongodb.org/browse/SERVER-11345
+        //ret.$set[prefix + 'avg'] = {$divide:[{$add: [{$mul: [prefix + 'avg', prefix + 'count']}, elapsed]}, {$add: [prefix + 'count', 1]}]};
+    });
+    return ret;
+};
+
+var TimeBasedMetric = function(host, path, method, year) {
+    var self = this;
+    self._id = [host, path, method, year].join(',');
+    self.id = function () {
+        return self._id;
+    };
+    self.updateNewDoc = function (month, hour, day, ua, statusCode, elapsed) {
+        self.host = host;
+        self.path = path;
+        self.method = method;
+        self.year = year;
+        self.type = 'combinedStats';
+        self = enrichWithStat(self);
+        self.months = newStatArr(12, 'm', true);
+        var daysInMonth = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        _.forEach(self.months, function (month) {
+            month.days = newStatArr(daysInMonth[month.m-1], 'd', true);
+            _.forEach(month.days, function (day) {
+                day.hours = newStatArr(24, 'h', false);
+            });
+        });
+        var stats = [self, self.months[month - 1], self.months[month - 1].days[day - 1],self.months[month - 1].days[day - 1].hours[hour]];
+        updNewDoc(stats, ua, statusCode, elapsed);
+        return self;
+    };
+    self.updateExistingDoc = function (month, day,hour, ua, statusCode, elapsed) {
+        var m = month - 1;
+        var d = day - 1;
+        var h = hour;
+        var prefixStr = ['',
+            'months.' + m + '.',
+            'months.' + m + '.days.' + d + '.',
+            'months.' + m + '.days.' + d + '.hours.' + h + '.'];
+        return updExistingDoc(prefixStr, ua, statusCode, elapsed);
+    };
+    return self;
+};
+
+var UserBasedMetric = function (host, path, method, user) {
+    var self = this;
+    self._id = [host, path, method, user].join(',');
+    self.id = function () {
+        return self._id;
+    };
+    self.updateNewDoc = function (month, hour, day, ua, statusCode, elapsed) {
+        self.host = host;
+        self.path = path;
+        self.method = method;
+        self.user = user;
+        self.type = 'userStats';
+        self = enrichWithStat(self);
+        var stats = [self];
+        updNewDoc(stats, ua, statusCode, elapsed);
+        return self;
+    };
+    self.updateExistingDoc = function (month, day, hour, ua, statusCode, elapsed) {
+        var prefixStr = [''];
+        return updExistingDoc(prefixStr, ua, statusCode, elapsed);
+    };
+    return self;
+};
+
 /* jshint unused: false*/
 var findAndUpdate = function (host, path, method, user, ua, year, month, day, hour, statusCode, elapsed) {
-    var queries = [
-        {
-            id: [host, path, method, year].join(','),
-            args: {host: host, path: path, method: method, year: year},
-            toUpdate: function (metric, user, year, month, day, hour) {
-                return [
-                    metric,
-                    metric.months[month - 1],
-                    metric.months[month - 1].days[day - 1],
-                    metric.months[month - 1].days[day - 1].hours[hour]
-                ];
-            }
-        },
-        {
-            id: [host, path, method, user].join(','),
-            args: {host: host, path: path, method: method, user: user},
-            toUpdate: function (metric, user, year, month, day, hour) {
-                return [metric];
-            }
-        }
-    ];
+    var queries = [new TimeBasedMetric(host, path, method, year), new UserBasedMetric(host, path, method, user)];
     var metrics = mongodb.collection('metrics');
     _.forEach(queries, function (query) {
-        metrics.findOne({_id: query.id}, function (err, doc) {
+        var toSave = null;
+        if (!cache[query.id()]) {
+            toSave = query.updateNewDoc(month, day, hour, ua, statusCode, elapsed);
+        } else {
+            toSave = query.updateExistingDoc(month, day, hour, ua, statusCode, elapsed);
+        }
+        metrics.findAndModify({_id: query.id()}, [['_id', 'asc']], toSave, {upsert: true, new: true}, function (err, doc) {
             if (err) {
-                //do nothing
+                console.log(err);
             } else {
-                var metric = doc || newDoc(query.args, query.id);
-                update(query.toUpdate(metric, user, year, month, day, hour), ua, statusCode, elapsed);
-                metrics.findAndModify({_id: metric._id}, [['_id', 'asc']], metric, {upsert: true}, function (err, doc) {
-                    if (err) {
-                        //do nothing;
-                    } else {
-                        //do nothing;
-                    }
-                });
+                if (doc) {
+                    cache[query.id()] = true;
+                }
             }
         });
     });

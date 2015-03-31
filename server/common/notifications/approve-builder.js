5c3afdabccfcb7e04220e22ac76a4e023f9a3875
ksheth <k.sheth@gmail.com>
2015-03-31 12:32:15 +0530

    will never need approvals for non restricted groups

--- server/common/notifications/approve-builder.js
+++ server/common/notifications/approve-builder.js
@@ -7,7 +7,7 @@ module.exports = function ApproveNotificationBuilder (toAdd, approvers, idForNot
             to: utils.hasItems(request.payload[toAdd]) ? obj[approvers] : [],
             title: ['{{title}} has new approved subscribers', {title: obj[idForNotificationsTitle]}],
             description: utils.hasItems(request.payload[toAdd]) ? {approved: request.payload[toAdd]} : {},
-            priority: obj.access === 'restricted' ? 'medium' : 'low'
+            priority: 'medium'
         };
     };
 };
